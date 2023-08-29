02187bdf9f10f2d660ed6a131dd756fa4aeaa10c
Ruben Polanco <andr3z.93@gmail.com>
2023-08-28 20:09:58 -0700

    push

--- problems/02-set-seconds-timeout-args.js
+++ problems/02-set-seconds-timeout-args.js
@@ -23,18 +23,17 @@ setSecondsTimeoutArgs(function(arg1, arg2) {
 ***********************************************************************/
 
 function setSecondsTimeoutArgs(cb, delayInSeconds, ...args) {
-  //setTimeout(cb, delayInSeconds * 1000, ...args)
-  setTimeout(cb(...args), delayInSeconds * 1000)
+  setTimeout(cb, delayInSeconds * 1000, ...args)
 }
 
-// function printSum(num1, num2, num3) {
-//   console.log(num1 + num2 + num3);
-// }
-// setSecondsTimeoutArgs(printSum, 0.25, 5, 1, 4); // should print '10' after 250ms
+function printSum(num1, num2, num3) {
+  console.log(num1 + num2 + num3);
+}
+setSecondsTimeoutArgs(printSum, 0.25, 5, 1, 4); // should print '10' after 250ms
 
-// setSecondsTimeoutArgs(function(arg1, arg2) {
-//   console.log(arg1 + '-' + arg2);
-// }, 0.7, 'hello', 'world'); // should print 'hello-world' after 700ms
+setSecondsTimeoutArgs(function(arg1, arg2) {
+  console.log(arg1 + '-' + arg2);
+}, 0.7, 'hello', 'world'); // should print 'hello-world' after 700ms
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
